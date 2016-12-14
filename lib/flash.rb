65b3bdaa95aa2682cd2f6d2fce202c985640ca92
gettinToasty <sean.r.beyer@gmail.com>
2016-12-13 17:34:37 -0800

    Add Flash

--- lib/flash.rb
+++ lib/flash.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'byebug'
 
 class Flash
 
@@ -6,23 +7,23 @@ class Flash
 
   def initialize(req)
     ivar = req.cookies['_rails_lite_app_flash']
-    @flash = ivar ? JSON.parse(ivar) : {}
-    @now = {}
+    @flash = {}
+    @now = ivar ? JSON.parse(ivar) : {}
   end
 
 
   def []=(key, val)
-    @flash[key.to_sym] = val || @now[key.to_sym] = val
+    @flash[key.to_s] = val
   end
 
   def [](key)
-    @flash[key.to_sym] || @now[key.to_sym]
+    @flash[key.to_s] || @now[key.to_s]
   end
 
   def store_flash(res)
     res.set_cookie('_rails_lite_app_flash',
                    path: '/',
-                   value: @flash.merge(@now).to_json
+                   value: @flash.to_json
                   )
   end
 end
